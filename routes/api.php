3f2b4c5a0a2bb2348eaaa85e644b371fae35a40d
feliximafidon <36160264+feliximafidon@users.noreply.github.com>
2021-03-06 00:02:47 +0100

    Update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'Halimtuhu\ArrayFiles\FieldController@index');
 Route::post('/upload', 'Halimtuhu\ArrayFiles\FieldController@upload');
-Route::delete('/delete/{image}', 'Halimtuhu\ArrayFiles\FieldController@delete');
+Route::delete('/delete/{image}', 'Halimtuhu\ArrayFiles\FieldController@delete')->where('image', '.*');
